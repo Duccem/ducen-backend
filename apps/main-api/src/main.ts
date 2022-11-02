b0cf00745b350cafc615c0d7d1516db81bc4f017
Jose <jose.veliz@rappi.com>
2022-11-02 16:31:49 -0400

    port use

--- apps/main-api/src/main.ts
+++ apps/main-api/src/main.ts
@@ -7,7 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(MainApiModule, {
     logger: new NestLogger(),
   });
-  console.log(process.env.PORT);
   app.use(
     session({
       secret: 'my-secret',
@@ -15,6 +14,6 @@ async function bootstrap() {
       saveUninitialized: false,
     }),
   );
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
